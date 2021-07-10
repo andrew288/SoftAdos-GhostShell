2381be47ad9ebdc181576fc544064268c18d5ba4
andrew <ajacoboc@unsa.edu.pe>
2021-07-10 12:14:37 -0500

    Creaccion de la app Public

---
+++ Backend/public/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from public import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view()),
+    path('data/Morbilidad_Adolescente',views.DataMorbilidadView.as_view()),
+    path('data/Riesgo_Adolescente',views.DataRiesgoView.as_view()),
+    path('data/Tamizaje_Adolescente',views.DataTamizajeView.as_view()),
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
