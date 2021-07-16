b079e71cfe03e7a838d1eae14d98de855b6b0294
bgomezv123 <bgomezv@unsa.edu.pe>
2021-07-15 23:24:53 -0500

    Se creo al app  articulos y su respectivo modelo

--- Backend/articulos/models.py
+++ Backend/articulos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Articulos(models.Model):
+    titulo = models.CharField(max_length=100, unique=False)
+    artfile = models.FileField(upload_to='documents/%Y/%m/%d', blank = False)
+
+
+
 # Create your models here.
